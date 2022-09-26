141a38ec462bdc74d0bb34d35ec01610a31c2a89
Fabricio C <fabriciocesaretti@gmail.com>
2022-09-26 10:06:49 -0300

    Design: new interactive home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { LanguageProvider } from './context/LanguageContext';
 import { LangToggler } from './components/LangToggler';
@@ -10,15 +10,27 @@ import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Socials } from './components/Socials';
 import { ScrollToTop } from './components/ScrollToTop';
+import { Profile } from './components/Profile';
+import { Reset } from './components/Reset';
 
 function App() {
+	const [count, setCount] = useState(0);
+	const [showReset, setShowReset] = useState(false);
+
+	const handleReset = () => {
+		setCount(count + 1);
+		setShowReset(false);
+	};
+
 	return (
 		<div className='App'>
 			<LanguageProvider>
 				<LangToggler />
 				<ThemeToggler />
 				<Navbar />
-				<Home />
+				{showReset && <Reset onClick={handleReset} />}
+				<Home key={count} setShowReset={setShowReset} />
+				<Profile />
 				<Skills />
 				<Projects />
 				<Contact />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,99 +1,68 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import '../styles/Home.css';
-import LanguageContext from '../context/LanguageContext';
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
-export const Home = () => {
-	const { language } = useContext(LanguageContext);
-	const fabricio = ['F', 'a', 'b', 'r', 'i', 'c', 'i', 'o'];
+export const Home = ({ setShowReset }) => {
+	const constraintsRef = useRef(null);
+
+	const fabricio = ['F', 'a', 'b', 'r', 'i', 'c', 'i', 'o', ' '];
 	const cesaretti = ['C', 'e', 's', 'a', 'r', 'e', 't', 't', 'i'];
-
-	const titleParent = {
-		hidden: {
-			opacity: 0,
-		},
-		show: {
-			opacity: 1,
-			transition: {
-				staggerChildren: 0.1,
-				delayChildren: 0.5,
-			},
-		},
-	};
-
-	const titleParent2 = {
-		hidden: {
-			opacity: 0,
-		},
-		show: {
-			opacity: 1,
-			transition: {
-				staggerChildren: 0.1,
-				delayChildren: 1.5,
-			},
-		},
-	};
-
-	const titleTextReveal = {
-		hidden: {
-			y: '120%',
-		},
-		show: {
-			y: '0%',
-		},
-	};
-
-	const transition = {
-		duration: 0.8,
-		ease: [0.6, -0.05, 0.01, 0.9],
-	};
 
 	return (
 		<div className='section__home' id='home'>
-			<motion.ol
-				className='name-wrapper'
-				variants={titleParent}
-				initial='hidden'
-				animate='show'
-			>
-				{fabricio.map((letter, index) => {
-					return (
-						<motion.li
-							key={index}
-							variants={titleTextReveal}
-							transition={transition}
-						>
-							{letter}
-						</motion.li>
-					);
-				})}
-			</motion.ol>
-			<motion.ol
-				className='name-wrapper'
-				variants={titleParent2}
-				initial='hidden'
-				animate='show'
-			>
-				{cesaretti.map((letter, index) => {
-					return (
-						<motion.li
-							key={index}
-							variants={titleTextReveal}
-							transition={transition}
-						>
-							{letter}
-						</motion.li>
-					);
-				})}
-			</motion.ol>
-			<motion.h2
-				className='home__h2'
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ duration: 1, delay: 3 }}
-			>
-				{language === 'es' ? 'Desarrollador Web' : 'Web Developer'}
-			</motion.h2>
+			<motion.div className='drag-area' ref={constraintsRef}>
+				<ol className='name-wrapper'>
+					{fabricio.map((letter, index) => {
+						return (
+							<motion.li
+								drag
+								dragConstraints={constraintsRef}
+								key={index}
+								whileTap={{
+									scale: 1.05,
+									textShadow: '0px 5px 8px #222',
+									cursor: 'grabbing',
+								}}
+								whileDrag={{ scale: 1.1, textShadow: '0px 10px 16px #222' }}
+								dragTransition={{
+									min: 0,
+									max: 100,
+									power: 0.5,
+								}}
+								onDragStart={() => setShowReset(true)}
+							>
+								{letter}
+							</motion.li>
+						);
+					})}
+				</ol>
+				<ol className='name-wrapper'>
+					{cesaretti.map((letter, index) => {
+						return (
+							<motion.li
+								drag
+								dragConstraints={constraintsRef}
+								key={index}
+								whileTap={{
+									scale: 1.05,
+									textShadow: '0px 5px 8px #222',
+									cursor: 'grabbing',
+								}}
+								whileDrag={{ scale: 1.1, textShadow: '0px 10px 16px #222' }}
+								dragTransition={{
+									min: 0,
+									max: 100,
+									power: 0.5,
+								}}
+								onDragStart={() => setShowReset(true)}
+							>
+								{letter}
+							</motion.li>
+						);
+					})}
+				</ol>
+			</motion.div>
 		</div>
 	);
 };
