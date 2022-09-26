2c8484532506f3f53e497b9dc586f8c8e0305e86
Fabricio C <fabriciocesaretti@gmail.com>
2022-09-26 09:51:47 -0300

    New: Profile section

---
+++ src/components/Profile.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react';
+import '../styles/Profile.css';
+import LanguageContext from '../context/LanguageContext';
+import ResumePDF from '../../public/Fabricio Cesaretti Resume.pdf';
+
+export const Profile = () => {
+	const { language } = useContext(LanguageContext);
+
+	return (
+		<div className='section__profile' id='profile'>
+			<div className='profile__description'>
+				{language === 'es' ? (
+					<p>
+						Hola! ✋ Soy <strong>PROGRAMADOR WEB</strong> especializado en
+						frontend. Principalmente uso React, pero me adapto bien y conozco
+						las tecnologías utilizadas en el desarrollo web moderno.
+					</p>
+				) : (
+					<p>
+						Hi! ✋ I am a <strong>WEB DEVELOPER</strong> specialized in the
+						frontend. I mainly use React, but I adapt well and I know the
+						technologies used in modern web development.
+					</p>
+				)}
+			</div>
+			<div className='profile__buttons'>
+				<a
+					href='#contact'
+					className='contact-btn'
+					title={`${language === 'es' ? 'Contactame' : 'Contact me'}`}
+				>
+					{language === 'es' ? 'Contactame' : 'Contact me'}
+				</a>
+				<a
+					href={ResumePDF}
+					className='cv-btn'
+					title={`${
+						language === 'es' ? 'Descargar Curriculum' : 'Download Resume'
+					}`}
+					download
+				>
+					{language === 'es' ? 'Descargar CV' : 'Download CV'}
+				</a>
+			</div>
+		</div>
+	);
+};
